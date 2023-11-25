4a644e65116a0a40a1a765b10ad9c237125ffde2
Ricky <rap555@nyu.edu>
2023-11-25 01:29:48 -0500

    further navbar improvements, skelton setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-black': '#14161b',
+      }
+    },
   },
   plugins: [],
 }
