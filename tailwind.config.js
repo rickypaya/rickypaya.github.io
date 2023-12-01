4f2e11b6efc847ab214f64933fff1e8fa9103478
Ricky <rap555@nyu.edu>
2023-11-30 20:41:16 -0500

    further work, started portfolio seciton

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,13 @@ module.exports = {
     extend: {
       colors: {
         'dark-black': '#14161b',
+        'grad-bg': '#999955',
+        'card-bg': '#E9E2D0',
       }
     },
+    boxShadow: {
+      'card-shadow': '-20px 0 35px -25px black, 20px 0 35px -25px black',
+    }
   },
   plugins: [],
 }
